253bd25573000bc9a274be988c226c2d1465b603
Jimmy Bates <jbates.productions@gmail.com>
2023-10-09 22:40:31 -0700

    Custom ok so far. no c

---
+++ mappings.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      { 
+        replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true
+      }
+    }
+  }
+}
+
+M.dap = {
+  plugin=true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
+  }
+}
+
+return M
