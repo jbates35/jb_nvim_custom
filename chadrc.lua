df1c8793e3f44401855b023380d352623bdd1eac
Jimmy Bates <jbates.productions@gmail.com>
2023-11-05 18:02:24 -0800

    Changed line number colors

--- chadrc.lua
+++ chadrc.lua
@@ -1,8 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'nightowl' }
+M.ui = { theme = 'yoru' }
 M.plugins="custom.plugins"
 M.mappings = require("custom.mappings")
+
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#DDEEFF'} )
 
 return M
