a721660c45de726b5aadc046a7441a4c00cb52fc
Jimmy Bates <jbates.productions@gmail.com>
2023-12-28 20:13:40 -0800

    added colouring

--- chadrc.lua
+++ chadrc.lua
@@ -6,5 +6,7 @@ M.plugins="custom.plugins"
 M.mappings = require("custom.mappings")
 
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#DDEEFF'} )
+vim.api.nvim_set_hl(0, 'Comment', { fg = '#666666'} )
+vim.api.nvim_set_hl(0, 'SpecialComment', { fg = '#666677'} )
 
 return M
